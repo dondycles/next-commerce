1cd56215bcf6b0ad184e5ff796bfa53e5c087276
dondycles <johnrod.dondoyano@gmail.com>
2023-08-28 01:14:34 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,22 +17,6 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#f9e4b8",
-          secondary: "#c3a5e5",
-          accent: "#a0ff47",
-          neutral: "#2b2839",
-          "base-100": "#342f41",
-          info: "#7995d2",
-          success: "#167e75",
-          warning: "#f4c11a",
-          error: "#e33564",
-        },
-      },
-      "light",
-      "dark",
-    ],
+    themes: ["light", "dark"],
   },
 };
