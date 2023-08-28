fe1458670a25dc51d25cea853bdbdcf56ae4a3f4
dondycles <johnrod.dondoyano@gmail.com>
2023-08-28 18:51:18 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,38 @@ module.exports = {
   },
   plugins: [require("daisyui"), require("tailwind-scrollbar")],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          primary: "blue",
+          "primary-focus": "mediumblue",
+        },
+      },
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          primary: "#aefcec",
+
+          secondary: "#67ea88",
+
+          accent: "#c8f990",
+
+          neutral: "#253341",
+
+          "base-100": "#39324e",
+
+          info: "#34a5cb",
+
+          success: "#0e6752",
+
+          warning: "#af5e0d",
+
+          error: "#fc2c33",
+        },
+      },
+      "light",
+      "dark",
+    ],
   },
 };
